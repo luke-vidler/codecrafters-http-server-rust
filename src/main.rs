87f4f0e02874d1e9efbfc152e2f68fddc85eeea4
Luke Vidler <luke.vidler@gmail.com>
2025-08-02 12:49:55 +1000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
-use std::io::Write;
-#[allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut mut_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let response_code = "HTTP/1.1 200 OK\r\n\r\n";
-                mut_stream.write(response_code.as_bytes()).unwrap();
+                handle_client(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let reader = BufReader::new(&stream);
+    let request = reader.lines().next().unwrap().unwrap();
+
+    let status_line = match &request[..] {
+        "GET / HTTP/1.1" => "HTTP/1.1 200 OK",
+        _ => "HTTP/1.1 404 Not Found",
+    };
+
+    let response = format!("{status_line}\r\n\r\n");
+    match stream.write_all(response.as_bytes()) {
+        Ok(_) => (),
+        Err(e) => println!("could not send response: {:?}", e),
+    };
 }
